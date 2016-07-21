1905a1f68b01aa9e233791afdb10c96bbd4de05c
hashimor <hashimor625@gmail.com>
2016-07-22 01:11:33 +0900

    カメラ目線スクリプト実装 ステージ大きさ変更

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private Transform myTran;
+    private Transform cameraTran;
+
+	void Start ()
+    {
+        myTran = transform;
+        cameraTran = Camera.main.transform;
+	}
+	
+	void Update ()
+    {
+        myTran.rotation = cameraTran.rotation;
+	}
+}
