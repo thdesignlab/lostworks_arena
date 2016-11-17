f57677783895512928b2caf5523095c7e5f389b9
takuyatommy <2.taku1031@gmail.com>
2016-11-18 04:30:50 +0900

    S武器にLaserLancher追加

--- Assets/Scripts/Common.cs
+++ Assets/Scripts/Common.cs
@@ -597,6 +597,7 @@ namespace Common
             { 3004, new string[]{ "EnergyShield", "", "", OBTAIN_TYPE_INIT}},
             { 3005, new string[]{ "ChargeArrow", "", "", OBTAIN_TYPE_INIT}},
             { 3006, new string[]{ "SearchRay", "", "", OBTAIN_TYPE_STORE}},
+            { 3007, new string[]{ "LaserLauncher", "", "", OBTAIN_TYPE_INIT }},
         };
         //背中武器(ダッシュ)リスト
         public static Dictionary<int, string[]> shoulderDashWeaponLineUp = new Dictionary<int, string[]>()
